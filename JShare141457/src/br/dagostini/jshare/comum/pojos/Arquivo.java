ddeac8daff8aa3725a3b8aa6445df81cd62a0a81
AlexandreNoro <xando10_noro@hotmail.com>
2016-04-28 22:08:42 -0300

    Implementado a classe ListarDiretoriosArquivos e atualizado a classe Arquivo

--- JShare141457/src/br/dagostini/jshare/comum/pojos/Arquivo.java
+++ JShare141457/src/br/dagostini/jshare/comum/pojos/Arquivo.java
@@ -1,5 +1,6 @@
 package br.dagostini.jshare.comum.pojos;
 
+import java.io.File;
 import java.io.Serializable;
 
 public class Arquivo implements Serializable {
@@ -8,7 +9,17 @@ public class Arquivo implements Serializable {
 
 	private String nome;
 	private long tamanho;
+	private File file;
 
+	
+	public File getFile(){
+		return file;
+	}
+	
+	public void setFile(File file){
+		this.file = file;
+	}
+	
 	public String getNome() {
 		return nome;
 	}
--- JShare141457/src/br/univel/jshare/ler/ListarDiretoriosArquivos.java
+++ JShare141457/src/br/univel/jshare/ler/ListarDiretoriosArquivos.java
@@ -1,5 +1,51 @@
 package br.univel.jshare.ler;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.dagostini.jshare.comum.pojos.Arquivo;
+import br.dagostini.jshare.comum.pojos.Diretorio;
+
 public class ListarDiretoriosArquivos {
 
+	private File dirStart = new File(".\\Share\\Download\\.");
+	
+	public List<Diretorio> listarDiretorios(){
+		
+		List<Diretorio> listaDiretorios = new ArrayList<>();
+		for (File file : dirStart.listFiles()) {
+			if (!file.isFile()) {
+				Diretorio dir = new Diretorio();
+				dir.setNome(file.getName());
+				listaDiretorios.add(dir);
+			}
+		}
+		
+		
+		return listaDiretorios;
+		
+	}
+	
+	public List<Arquivo> listarArquivos(){
+		
+		List<Arquivo> listaArquivos = new ArrayList<>();
+		
+		for (File file : dirStart.listFiles()) {
+			if (file.isFile()) {
+				Arquivo arq = new Arquivo();
+				
+				arq.setNome(file.getName());
+				arq.setTamanho(file.length());
+				arq.setFile(file);
+				listaArquivos.add(arq);
+			}
+		}
+		
+		
+		return listaArquivos;
+		
+	}
+	
+	
 }
