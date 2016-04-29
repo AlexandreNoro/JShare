ab05c1eaa7064f3c30bc76a58e0d6a791106127f
AlexandreNoro <xando10_noro@hotmail.com>
2016-04-28 21:48:36 -0300

    Criado pacote jshare.ler e as respectivas classes. Implementado a classe LeituraEscritadeArquivos até o momento.

---
+++ JShare141457/src/br/univel/jshare/ler/LeituraEscritadeArquivos.java
@@ -0,0 +1,56 @@
+package br.univel.jshare.ler;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class LeituraEscritadeArquivos {
+
+	public static void main(String[] args) {
+
+		String nomeArquivo = "DadosAluno.txt";
+
+		new LeituraEscritadeArquivos(new File(".\\Share\\Download\\" + nomeArquivo));
+	}
+
+	public LeituraEscritadeArquivos() {
+	}
+
+	public LeituraEscritadeArquivos(File f) {
+
+		System.out.println("Início da leitura ");
+
+		byte[] dados = leia(f);
+
+		System.out.println("Início da copia ");
+
+		escreva(new File(".\\Share\\Upload\\" + " Cópia de " + f.getName()), dados);
+
+	}
+
+	private void escreva(File arq, byte[] dados) {
+
+		try {
+			Files.write(Paths.get(arq.getPath()), dados, StandardOpenOption.CREATE);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	private byte[] leia(File arq) {
+
+		Path path = Paths.get(arq.getPath());
+
+		try {
+			byte[] dados = Files.readAllBytes(path);
+			return dados;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
---
+++ JShare141457/src/br/univel/jshare/ler/ListarDiretoriosArquivos.java
@@ -0,0 +1,5 @@
+package br.univel.jshare.ler;
+
+public class ListarDiretoriosArquivos {
+
+}
